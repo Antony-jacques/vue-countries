238dd23d0b01f4ff83ac2fb1645681af23009d51
Antony <antony.jacques@laposte.net>
2023-02-09 19:33:45 +0100

    get data

--- src/views/CountriesListView.vue
+++ src/views/CountriesListView.vue
@@ -4,6 +4,18 @@
 <script>
 export default {
     name: 'countriesListView',
+    mounted(){
+        try {
+            fetch('https://restcountries.com/v3.1/all')
+            .then(response => response.json())
+            .then(data => console.log(data))
+        } catch (error){
+           console.log(error); 
+        }
+
+
+    }
+
 
 }
 
