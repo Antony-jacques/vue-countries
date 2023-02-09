d1e12d96614c0bfb5655168b03849b87c33e91be
Antony <antony.jacques@laposte.net>
2023-02-09 20:15:44 +0100

    toggledetails visibility

--- src/components/CountryItem.vue
+++ src/components/CountryItem.vue
@@ -1,27 +1,40 @@
 <template>
-    <div>
-        <div class="name">
-            Country: {{ country.name.official}}
-        </div>
-        <div class="image-wrapper">
-            <img :src=country.flags.svg alt="">
-        </div>
+  <div @click="toggleDetails()">
+    <div class="name">Country: {{ country.name.official}}</div>
+    <div class="image-wrapper">
+      <img :src="country.flags.svg" alt />
     </div>
+
+    <div v-if="isVisible" class="details">
+        details
+      <!-- diplay country details in a specific component or create a specific view that display a single component -> vue router param + api /name endpoint
+      -->
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CountryItem',
-    props: {
-        country: Object
+  name: "CountryItem",
+  props: {
+    country: Object
+  },
+  data() {
+    return {
+      isVisible: false
+    };
+  },
+  methods: {
+    toggleDetails: function() {
+      this.isVisible = !this.isVisible;
     }
-}
-
+  }
+};
 </script>
 
-<style>
+<style scoped>
 img {
-    width:200px
+  width: 200px;
 }
-
 </style>
+
